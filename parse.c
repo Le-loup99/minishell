bb04c9fa0602118836f0d348eccc1a3428e75903
Le-loup99 <amramamandresy@gmail.com>
2025-11-05 13:12:50 +0300

    le brebis
    Dieu merci
    cela fonctionne bien juste quelque retouche et ca va etre ok

--- parse.c
+++ parse.c
@@ -49,7 +49,7 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-char *get_cmd(char *str, char *tmp, int index)
+char *get_cmd(char *str, char *tmp)
 {
 	int	i;
 	int	j;
@@ -58,76 +58,140 @@ char *get_cmd(char *str, char *tmp, int index)
 	i = 0;
 	quote = 0;
 	j = 0;
-	if (tmp)
-		free(tmp);
-	tmp = malloc (1 * ft_strlen(str) + 1);
 	while(str[i])
 	{
-		if (tmp[0] && (str[i] == '\'' || str[i] == '"'))
+		if (tmp[0] && (str[i] == '\'' || str[i] == '"') && i != 0)
 		{
-			printf("%s", tmp);
-			return (tmp);
+			if ((str[i + 1] == ' ' || !str[i + 1]))
+			{
+				printf("%s", tmp);
+				return (tmp);
+			}
+			quote = 0;
+			if (str[i] == '\'' || str[i] == '"')
+			{
+				if (str[i] == '\'')
+					quote = 1;
+				else
+					quote = 2;
+				i++;
+			}
 		}
 		if (str[i] == '"' && quote == 0)
 		{
 			quote = 2;
-			index++;
 			i++;
 		}
 		else if (str[i] == '\'' && quote == 0)
 		{
-			index++;
 			quote = 1;
 			i++;
 		}
 		if (str[i] == '"' && quote == 2)
 		{
-			index++;
-			printf("%s", tmp);
-			return (tmp);
+			if (str[i + 1] == ' ' || str[i + 1] == 0)
+			{
+				printf("%s", tmp);
+				return (tmp);
+			}
+			i++;
 		}
 		else if (str[i] == '\'' && quote == 1)
 		{
-			index++;
-			printf("%s", tmp);
-			return (tmp);
+			if (str[i + 1] == ' ' || str[i + 1] == 0)
+			{
+				printf("%s", tmp);
+				return (tmp);
+			}
+			i++;
 		}
-		if (str[i] == ' ' && quote == 0)
+		if (str[i] == '"' && quote == 0)
 		{
-			index++;
-			printf("%s", tmp);
-			return (tmp);
+			quote = 2;
+			i++;
 		}
+		// else if (str[i] == '\'' && quote == 0)
+		// {
+		// 	quote = 1;
+		// 	i++;
+		// }
+		// if (str[i] == ' ' && quote == 0)
+		// {
+		// 	printf("eto %s", tmp);
+		// 	return (tmp);
+		// }
 		tmp[j] = str[i];
-		index++;
 		j++;
-		i++;
+		if (str[i])
+			i++;
 	}
 	printf("%s", tmp);
 	return (tmp);
 }
 
-char *cmd(char *str)
+void	move_index(char *str, int *i)
+{
+	while(str[*i] == ' ')
+		(*i)++;
+	if (str[*i] == '"')
+	{
+		(*i)++;
+		while (str[*i] && str[*i] != '"')
+			(*i)++;
+		(*i) += 1;
+		return ;
+	}
+	else if (str[*i] == '\'')
+	{
+		(*i)++;
+		while (str[*i] && str[*i] != '\'')
+			(*i)++;
+		return ;
+	}
+	else
+	{
+		while (str[*i] && str[*i] != ' ' && str[*i] != '\'' && str[*i] != '"')
+			(*i)++;
+	}
+	return ;
+}
+
+char **cmd(char *str)
 {
 	char	*tmp;
 	char	**all_cmd;
 	int		i;
+	int		stock_index;
 
+	stock_index = 0;
 	i = 0;
-	all_cmd = malloc(sizeof(char) * count_cmd(str));
+	all_cmd = malloc(sizeof(char *) * count_cmd(str));
 	while (str[i])
 	{
+		tmp = malloc(sizeof(char ) * ft_strlen(str));
 		while (str[i] == ' ')
 			i++;
-		tmp = get_cmd(str, tmp, i);
+		get_cmd(str + i, tmp);
 		printf("%s\n", tmp);
+		move_index(str, &i);
+		all_cmd[stock_index] = tmp;
+		stock_index++;
 	}
+	i = 0;
+	while (*all_cmd[i])
+	{
+		printf("\n%s", all_cmd[i]);
+		i++;
+	}
+	return(all_cmd);
 }
 int main()
 {
-	char *str = "\"ab\" 'bombo clat'";
+	char *str = "\"ab\" kozy' bombo clat'";
 	char *tmp;
 
 	tmp = NULL;
-	get_cmd(str, tmp, 0);
+	cmd(str);
 }
+
+// next test id ""kozy\" kaiza\" 
