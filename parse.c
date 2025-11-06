877d8523767473ce4c48de49a6135feaa6fdc059
Le-loup99 <amramamandresy@gmail.com>
2025-11-06 17:11:05 +0300

    Un brebis
    Dieu merci !
    on a beaucoup avance aujourd'hui mais il faut un traitement special pour '' et ""

--- parse.c
+++ parse.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arakoto2 <arakoto2@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 10:54:52 by arakoto2          #+#    #+#             */
+/*   Updated: 2025/11/06 17:09:07 by arakoto2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -49,112 +61,204 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-char *get_cmd(char *str, char *tmp)
+int	 operator_alone_in_quote(char *str, int i)
 {
-	int	i;
+	if(str[i] == '\'' && (str[i + 1] == '<' || str[i + 1] == '>'))
+	{
+		if (str[i + 2] == '\'' && (str[i + 3] == ' ' || str[i + 3] == 0))
+			return (1);
+		else if((str[i + 2] == '<' || str[i + 2] == '>') && (str[i + 3] == '\'') && (str[i + 4] == ' ' || str[i + 4] == 0))
+			return (1);
+	}
+	else if(str[i] == '"' && (str[i + 1] == '<' || str[i + 1] == '>'))
+	{
+		if (str[i + 2] == '"' && (str[i + 3] == ' ' || str[i + 3] == 0))
+			return (1);
+		else if((str[i + 2] == '<' || str[i + 2] == '>') && (str[i + 3] == '"') && (str[i + 3] == ' ' || str[i + 3] == 0))
+			return (1);
+	}
+	return (0);
+}
+
+void copy_operator_in_quote(char *str, char *tmp, int *i)
+{
+	int	j;
+
+	j = 0;
+	tmp[j] = str[*i];
+	(*i)++;
+	j++;
+	while (j < 4)
+	{
+		if (str[*i] == tmp[0])
+		{
+			tmp[j] = str[*i];
+			(*i)++;
+			return ;
+		}
+		tmp[j] = str[*i];
+		j++;
+		(*i)++;
+	}
+	return ;
+}
+
+char *get_cmd(char *str, char *tmp, int *i)
+{
 	int	j;
 	int quote;
 
-	i = 0;
 	quote = 0;
 	j = 0;
-	while(str[i])
+	if (operator_alone_in_quote(str, (*i)))
 	{
-		if (tmp[0] && (str[i] == '\'' || str[i] == '"') && i != 0)
+		printf("niditra");
+		copy_operator_in_quote(str, tmp, i);
+		return (tmp);
+	}
+	while (str[*i] == '<' || str[*i] == '>')
+	{
+		tmp[j] = str[*i];
+		(*i)++;
+		j++;
+	}
+	if (j > 0)
+		return (tmp);
+	while(str[*i])
+	{
+		if (tmp[0] && (str[*i] == '\'' || str[*i] == '"') && *i != 0)
 		{
-			if ((str[i + 1] == ' ' || !str[i + 1]))
+			if ((str[(*i) + 1] == ' ' || !str[(*i) + 1]) && str[(*i) - 1] == ' ')
 			{
+				printf("%s", tmp);
+				return (tmp);
+			}
+			if ((quote == 1 && str[*i] == '\'') || (quote == 2 && str[*i] == '"'))
+			{
+				quote = 0;
+				(*i)++;
+			}
+			else if (quote == 0 && str[(*i)] == '\'')
+			{
+				quote = 1;
+				(*i)++;
+			}
+			else if (quote == 0 && str[*i] == '"')
+			{
+				quote = 2;
+				(*i)++;
+			}
+			else
+			{
+				tmp[j] = str[(*i)];
+				(*i)++;
+				j++;
+			}
+		}
+		if (str[*i] == '"' && quote == 0)
+		{
+			quote = 2;
+			(*i)++;
+		}
+		else if (str[*i] == '\'' && quote == 0)
+		{
+			quote = 1;
+			(*i)++;
+		}
+		if (str[*i] == '"' && quote == 2)
+		{
+			if (str[(*i) + 1] == ' ' || str[(*i) + 1] == 0)
+			{
+				(*i)++;
 				printf("%s", tmp);
 				return (tmp);
 			}
 			quote = 0;
-			if (str[i] == '\'' || str[i] == '"')
-			{
-				if (str[i] == '\'')
-					quote = 1;
-				else
-					quote = 2;
-				i++;
-			}
+			(*i)++;
 		}
-		if (str[i] == '"' && quote == 0)
+		else if (str[*i] == '\'' && quote == 1)
+		{
+			if (str[(*i) + 1] == ' ' || str[(*i) + 1] == 0)
+			{
+				(*i)++;
+				printf("%s", tmp);
+				return (tmp);
+			}
+			quote = 0;
+			(*i)++;
+		}
+		if (str[*i] == '"' && quote == 0)
 		{
 			quote = 2;
-			i++;
+			(*i)++;
 		}
-		else if (str[i] == '\'' && quote == 0)
+		else if (str[*i] == '\'' && quote == 0)
 		{
 			quote = 1;
-			i++;
+			(*i)++;
 		}
-		if (str[i] == '"' && quote == 2)
+		if ((str[*i] == ' ' || (str[*i] == '<' || str[*i] == '>')) && quote == 0)
 		{
-			if (str[i + 1] == ' ' || str[i + 1] == 0)
+			printf("eto %s", tmp);
+			return (tmp);
+		}
+		/*if ((str[((*i) - 1)] == '\'' || str[(*i) - 1] == '"') || (str[*i] == '<' && str[*i] == '>'))
+		{
+			if (operator_alone_in_quote(str, (*i) - 1))
 			{
-				printf("%s", tmp);
-				return (tmp);
+				(*i)--;
+				get_cmd(str, tmp, i);
 			}
-			i++;
-		}
-		else if (str[i] == '\'' && quote == 1)
+		}*/
+		if ((str[*i] != '\'' || quote != 1) && (str[*i] != '"' || quote != 2))
 		{
-			if (str[i + 1] == ' ' || str[i + 1] == 0)
-			{
-				printf("%s", tmp);
-				return (tmp);
-			}
-			i++;
+			tmp[j] = str[*i];
+			j++;
+			if (str[*i])
+				(*i)++;
 		}
-		if (str[i] == '"' && quote == 0)
-		{
-			quote = 2;
-			i++;
-		}
-		// else if (str[i] == '\'' && quote == 0)
-		// {
-		// 	quote = 1;
-		// 	i++;
-		// }
-		// if (str[i] == ' ' && quote == 0)
-		// {
-		// 	printf("eto %s", tmp);
-		// 	return (tmp);
-		// }
-		tmp[j] = str[i];
-		j++;
-		if (str[i])
-			i++;
 	}
 	printf("%s", tmp);
 	return (tmp);
 }
 
-void	move_index(char *str, int *i)
-{
-	while(str[*i] == ' ')
-		(*i)++;
-	if (str[*i] == '"')
-	{
-		(*i)++;
-		while (str[*i] && str[*i] != '"')
-			(*i)++;
-		(*i) += 1;
-		return ;
-	}
-	else if (str[*i] == '\'')
-	{
-		(*i)++;
-		while (str[*i] && str[*i] != '\'')
-			(*i)++;
-		return ;
-	}
-	else
-	{
-		while (str[*i] && str[*i] != ' ' && str[*i] != '\'' && str[*i] != '"')
-			(*i)++;
-	}
-	return ;
-}
+// void	move_index(char *str, int *i, int check)
+// {
+// 	while(str[*i] == ' ')
+// 		(*i)++;
+// 	if (str[*i] == '"')
+// 	{
+// 		(*i)++;
+// 		while (str[*i] && str[*i] != '"' && (str[*i] != ' ' || !(check % 2)))
+// 			(*i)++;
+// 		if (str[*i] && str[(*i) + 1] != ' ' && (str[*i] != ' ' || !(check % 2)))
+// 			move_index(str , i, ++check);
+// 		if (str[*i] == '"')
+// 			(*i) += 1;
+// 		return ;
+// 	}
+// 	else if (str[*i] == '\'')
+// 	{
+// 		(*i)++;
+// 		while (str[*i] && str[*i] != '\'' && (str[*i] != ' ' || !(check % 2)))
+// 			(*i)++;
+// 		// if (str[*i] && str[(*i) + 1] != ' ')
+// 		// 	move_index(str, i, ++check);
+// 		if (str[*i] && str[(*i) + 1] != ' ' && (str[*i] != ' ' || !(check % 2)))
+// 			move_index(str , i, ++check);
+// 		if (str[*i] == '\'')
+// 			(*i) += 1;
+// 		return ;
+// 	}
+// 	else
+// 	{
+// 		while (str[*i] && str[*i] != ' ' && str[*i] != '\'' && str[*i] != '"')
+// 			(*i)++;
+// 		if (str[*i] == '\'' || str[*i] == '"')
+// 			move_index(str, i, ++check);
+// 	}
+// 	return ;
+// }
 
 char **cmd(char *str)
 {
@@ -171,27 +275,32 @@ char **cmd(char *str)
 		tmp = malloc(sizeof(char ) * ft_strlen(str));
 		while (str[i] == ' ')
 			i++;
-		get_cmd(str + i, tmp);
+		get_cmd(str, tmp, &i);
 		printf("%s\n", tmp);
-		move_index(str, &i);
+		// move_index(str, &i, 0);
 		all_cmd[stock_index] = tmp;
 		stock_index++;
 	}
 	i = 0;
-	while (*all_cmd[i])
-	{
-		printf("\n%s", all_cmd[i]);
-		i++;
-	}
+	// while (*all_cmd[i])
+	// {
+		printf("\n0 = %s", all_cmd[0]);
+		printf("\n1 = %s", all_cmd[1]);
+		printf("\n2 = %s", all_cmd[2]);
+		printf("\n3 = %s", all_cmd[3]);
+		printf("\n4 = %s", all_cmd[4]);
+		printf("\n5 = %s", all_cmd[6]);
+	// 	i++;
+	// }
 	return(all_cmd);
 }
 int main()
 {
-	char *str = "\"ab\" kozy' bombo clat'";
+	char *str = "ls'' <''| grep kaiza";
 	char *tmp;
 
 	tmp = NULL;
 	cmd(str);
 }
-
-// next test id ""kozy\" kaiza\" 
+// "ls -l ''<''| grep kaiza"
+// ''
